ee0431d00fe6e1255503500329d0506fed19a6e5
Caiocr8 <ccaiocr@gmail.com>
2023-03-23 16:32:20 -0300

    ROUTES

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/login/index';
+import Register from './pages/register/index';
+import TodoList from './pages/todolist/index';
 import './index.css';
-import App from './App';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route path='/home' element={<TodoList />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
